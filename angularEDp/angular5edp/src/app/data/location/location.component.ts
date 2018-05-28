b5eb17b8ef3abd6afe49e12e2b6287467487ad7c
Aniket <aniket57gholap22@gmail.com>
2018-05-28 13:10:12 +0530

    table

--- angularEDp/angular5edp/src/app/data/location/location.component.ts
+++ angularEDp/angular5edp/src/app/data/location/location.component.ts
@@ -1,21 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,ViewEncapsulation,ViewChild} from '@angular/core';
 import {Router} from "@angular/router";
 import { DataService } from '../data.service';
-
+import { JsonApiService } from "app/core/api/json-api.service";
+import {ModalDirective} from "ngx-bootstrap";
 @Component({
   selector: 'app-loction',
   templateUrl: './location.component.html',
+  styleUrls: ['./location.component.css','../../smart-admin.css'],
+  encapsulation: ViewEncapsulation.None,
   providers: [DataService],
-  styleUrls:['./location.component.css']
+  
 })
 export class LocationComponent implements OnInit {
   
-  constructor(private router: Router,private dataService: DataService) { }
-
+  rows: any[] = [];
+  temp:any[]=[]
+  controls={filter:''}
+  constructor(private jsonApiService:JsonApiService) { }
+   @ViewChild('myTable') table: any;
+   @ViewChild('staticModal') public staticModal:ModalDirective;
   ngOnInit() {
- 
+    this.jsonApiService.fetch('/tables/datatables.filters.json').subscribe(data=> {
+      this.rows = data;
+      this.temp=data;
+      console.log(data)
+    })
   }
-  
-   
 
+
+   updateFilter(event) {
+    const val = event.target.value.toLowerCase();
+
+    // filter our data
+    const temp = this.temp.filter(function(d) {
+      return !val || ['name', 'age', 'office'].some((field: any)=>{
+        return d[field].toLowerCase().indexOf(val) !== -1
+      }) 
+    });
+
+    // update the rows
+    this.rows = temp;
+    // Whenever the filter changes, always go back to the first page
+    this.table.offset = 0;
+  }
+
+  
+
+
+  toggleExpandRow(row) {
+    console.log('Toggled Expand Row!', row);
+    this.table.rowDetail.toggleExpandRow(row);
+  }
+
+  onDetailToggle(event) {
+    console.log('Detail Toggled', event);
+  }
+  get(row){
+    console.log(row)
+  }
+  edit(row){
+     console.log(row)
+  }
+  delete(row){
+     console.log(row)
+  }
 }
--- angularEDp/angular5edp/src/app/data/location/location.module.ts
+++ angularEDp/angular5edp/src/app/data/location/location.module.ts
@@ -3,12 +3,22 @@ import { CommonModule } from '@angular/common';
 import { LocationRoutingModule } from './location-routing.module';
 import { LocationComponent } from './location.component';
 import { FormsModule } from '@angular/forms';
-
+import {SmartadminDatatableModule} from "../../shared/ui/datatable/smartadmin-datatable.module";
+import {NgxDatatableModule} from "@swimlane/ngx-datatable";
+import {SmartadminModule} from "../../shared/smartadmin.module";
+import {AccordionModule, CarouselModule,ModalModule} from "ngx-bootstrap";
 @NgModule({
   imports: [
     CommonModule,
     LocationRoutingModule,
-    FormsModule
+    FormsModule,
+    SmartadminModule,
+      NgxDatatableModule,
+      SmartadminDatatableModule,
+      AccordionModule.forRoot(),
+      ModalModule.forRoot(),
+      CarouselModule.forRoot(),
+
   ],
   declarations: [LocationComponent]
 })
